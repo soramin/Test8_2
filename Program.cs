e38a8f176959f3e5c43bfde4d6d58e572de5c3a1
soramin <44796681+soramin@users.noreply.github.com>
2020-05-12 19:33:36 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Test8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            int result = 0;
+
+            Console.WriteLine("整数1 = ");
+            string strNo1 = Console.ReadLine();
+
+            Console.WriteLine("整数2 = ");
+            string strNo2 = Console.ReadLine();
+
+            try
+            {
+                x = int.Parse(strNo1);
+                y = int.Parse(strNo2);
+
+                result = x / y;
+                Console.WriteLine($"割り算の結果は {result} です。");
+            }
+
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("ゼロで割ることはできません。");
+            }
+
+            finally
+            {
+                Console.WriteLine("処理を終了します。");
+            }
+        }
+    }
+}
